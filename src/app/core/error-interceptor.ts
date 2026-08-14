0e2941da67972c9463fb50c322d652fc2492e03c
Antonio <antojoseabad@gmail.com>
2026-08-14 14:26:40 +0200

    feat(core): add HTTP error interceptor

---
+++ src/app/core/error-interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      console.error(
+        `HTTP Error: ${error.status} on ${req.method} ${req.url}:`,
+        error.error?.message ?? error.message,
+      );
+      return throwError(() => error);
+    }),
+  );
+};
