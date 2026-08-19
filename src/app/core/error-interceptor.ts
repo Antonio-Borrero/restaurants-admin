3b918511ae263b9f3bfea05331e03a16b1521603
Antonio <antojoseabad@gmail.com>
2026-08-19 17:43:03 +0200

    feat(core): update error interceptor to redirect to login when token is invalid

--- src/app/core/error-interceptor.ts
+++ src/app/core/error-interceptor.ts
@@ -1,9 +1,19 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { Auth } from './auth';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(Auth);
+  const router = inject(Router);
+
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        auth.logout();
+        router.navigate(['/login']);
+      }
       console.error(
         `HTTP Error: ${error.status} on ${req.method} ${req.url}:`,
         error.error?.error?.message ?? error.message,
