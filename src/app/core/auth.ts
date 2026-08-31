48cedc2060efb3f4a959b73cfd716da04f911d23
Antonio <antojoseabad@gmail.com>
2026-08-31 15:57:26 +0200

    feat(core): expose authenticated user's email via signal

--- src/app/core/auth.ts
+++ src/app/core/auth.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { tap } from 'rxjs';
 
@@ -8,18 +8,26 @@ import { tap } from 'rxjs';
 })
 export class Auth {
   private http = inject(HttpClient);
+  private email = signal(localStorage.getItem('email'));
+  userEmail = this.email.asReadonly();
 
   login(email: string, password: string) {
     return this.http
-      .post<{ token: string }>(`${environment.apiUrl}/auth/login`, { email, password })
+      .post<{
+        token: string;
+      }>(`${environment.apiUrl}/auth/login`, { email, password })
       .pipe(
         tap((resp) => {
+          this.email.set(email);
+          localStorage.setItem('email', email);
           localStorage.setItem('token', resp.token);
         }),
       );
   }
 
   logout() {
+    this.email.set(null);
+    localStorage.removeItem('email');
     localStorage.removeItem('token');
   }
 
