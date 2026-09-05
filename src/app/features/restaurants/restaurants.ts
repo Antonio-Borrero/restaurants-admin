ed9149b12723b74d5b2b8e8294a21cafeb1693f7
Antonio <antojoseabad@gmail.com>
2026-09-05 16:45:21 +0200

    feat(restaurants): add grid / list view modes

--- src/app/features/restaurants/restaurants.ts
+++ src/app/features/restaurants/restaurants.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RestaurantsService } from './restaurants-service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { InitialsPipe } from './initials-pipe';
 import { UpperCasePipe } from '@angular/common';
+
+type ViewMode = 'grid' | 'list';
 
 @Component({
   selector: 'app-restaurants',
@@ -12,8 +14,13 @@ import { UpperCasePipe } from '@angular/common';
 })
 export class Restaurants {
   private restaurantsService = inject(RestaurantsService);
+  protected viewMode = signal<ViewMode>('grid');
 
   protected restaurantsList = toSignal(this.restaurantsService.getRestaurants(), {
     initialValue: [],
   });
+
+  protected changeViewMode(viewMode: ViewMode) {
+    this.viewMode.set(viewMode);
+  }
 }
