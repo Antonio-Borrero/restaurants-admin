8956b640483fd43f799c31ed2ce4a5f503e7fdc9
Antonio <antojoseabad@gmail.com>
2026-09-07 23:33:06 +0200

    feat(shared): make layout topbar responsive

--- src/app/shared/layout/layout.ts
+++ src/app/shared/layout/layout.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { Logo } from '../logo/logo';
 import { Auth } from '../../core/auth';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -13,6 +16,14 @@ export class Layout {
   protected auth = inject(Auth);
   protected email = this.auth.userEmail;
   private router = inject(Router);
+  private breakpointObserver = inject(BreakpointObserver);
+
+  protected logoWidth = toSignal(
+    this.breakpointObserver
+      .observe('(max-width: 768px)')
+      .pipe(map((result) => (result.matches ? 45 : 80))),
+    { initialValue: 80 },
+  );
 
   logout() {
     this.auth.logout();
