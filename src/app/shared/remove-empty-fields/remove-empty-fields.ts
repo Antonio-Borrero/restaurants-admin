25a3ec51b75836ef018b78299915b99962623604
Antonio <antojoseabad@gmail.com>
2026-09-14 05:59:41 +0200

    feat(shared): add utility to remove empty fields

---
+++ src/app/shared/remove-empty-fields/remove-empty-fields.ts
@@ -0,0 +1,3 @@
+export function removeEmptyFields<T extends Record<string, unknown>>(obj: T): Partial<T> {
+  return Object.fromEntries(Object.entries(obj).filter(([, value]) => value !== '')) as Partial<T>;
+}
