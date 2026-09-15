a27b00cbbdb26f7612ceaaafc56bb629df4963e4
Antonio <antojoseabad@gmail.com>
2026-09-15 16:23:06 +0200

    feat(menu): add createCategory method to MenuService

--- src/app/features/menu/menu-service.ts
+++ src/app/features/menu/menu-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { RestaurantMenu } from '../../shared/menu-interface/menu-interface';
+import { Category, RestaurantMenu } from '../../shared/menu-interface/menu-interface';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +13,11 @@ export class MenuService {
     return this.http.get<RestaurantMenu>(
       `${environment.apiUrl}/restaurants/${restaurantId}/menu?locale=${locale}`,
     );
+  }
+
+  createCategory(restaurantId: string, name: string, locale: string) {
+    return this.http.post(`${environment.apiUrl}/restaurants/${restaurantId}/categories`, {
+      translations: [{ name, locale }],
+    });
   }
 }
