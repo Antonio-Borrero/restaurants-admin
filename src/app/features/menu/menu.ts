fabb78a128c058863a9e83bb3c96b07e0b6796b9
Antonio <antojoseabad@gmail.com>
2026-09-15 16:45:31 +0200

    refactor(menu): update method to fetch menu from switchmap to signal

--- src/app/features/menu/menu.ts
+++ src/app/features/menu/menu.ts
@@ -1,16 +1,19 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MenuService } from './menu-service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { map, switchMap } from 'rxjs';
+import { map } from 'rxjs';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { breakpointMobile } from '../../shared/breakpoints/breakpoints';
 import { MenuDesktop } from './menu-desktop/menu-desktop';
 import { MenuMobile } from './menu-mobile/menu-mobile';
+import { RestaurantMenu } from '../../shared/menu-interface/menu-interface';
+import { DEFAULT_LOCALE } from '../../core/default-locale';
+import { CategoryForm } from './category-form/category-form';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink, MenuDesktop, MenuMobile],
+  imports: [RouterLink, MenuDesktop, MenuMobile, CategoryForm],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
@@ -22,6 +25,18 @@ export class Menu {
   protected dishCount = computed(() =>
     this.menu()?.categories.reduce((count, category) => count + category.dishes.length, 0),
   );
+  protected menu = signal<RestaurantMenu | null>(null);
+  private restaurantId = '';
+
+  constructor() {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        const id = params.get('id')!;
+        this.restaurantId = id;
+        this.getRestaurantMenu();
+      },
+    });
+  }
 
   protected isMobile = toSignal(
     this.breakpointObserver
@@ -30,12 +45,11 @@ export class Menu {
     { initialValue: true },
   );
 
-  protected menu = toSignal(
-    this.route.paramMap.pipe(
-      switchMap((param) => {
-        const id = param.get('id')!;
-        return this.menuService.getRestaurantMenu(id, 'es');
-      }),
-    ),
-  );
+  protected getRestaurantMenu() {
+    this.menuService.getRestaurantMenu(this.restaurantId, DEFAULT_LOCALE).subscribe({
+      next: (resp) => {
+        this.menu.set(resp);
+      },
+    });
+  }
 }
